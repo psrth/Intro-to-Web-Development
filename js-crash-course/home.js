302487624cfb3a34441f2eecd3be97630a75d036
Parth Sharma <parthsharma151@gmail.com>
2020-06-23 13:07:39 +0530

    console, alerts, prompts, numbers, and variables in JS

---
+++ js-crash-course/home.js
@@ -0,0 +1,54 @@
+// -----------------------------------------------------
+
+
+// CONSOLE LOGS, PROMPTS, AND ALERTS
+
+// // Logs the message in browser's console
+// console.log('Hello.');
+
+// // the webpage sends out an alert as a banner
+// alert('whats good fam');
+
+
+// -----------------------------------------------------
+
+
+// VARIABLES IN JS are declared using var
+
+// // this is a string variable
+// var i = 'imma variable';
+// console.log(i);
+
+// // this is a number variable
+// var num = 365;
+// console.log(num);
+
+// var age = prompt('What is your age?');
+
+// // editing the inner html of an element
+// document.getElementById('ageFam').innerHTML = age;
+
+
+// -----------------------------------------------------
+
+
+// NUMBERS IN JAVA SCRIPT
+
+// var num1 = 5.7;
+// console.log(50 / 5);
+
+// // inline increment
+// console.log((++num1));
+
+// Divide by /, Multiple by *, Remainder by *
+
+// // Incremement/decrement by a particular number
+// var num = 10;
+// num += 20;
+// console.log(num);
+
+
+// -----------------------------------------------------
+
+
+// FUNCTIONS IN JAVASCRIPT
